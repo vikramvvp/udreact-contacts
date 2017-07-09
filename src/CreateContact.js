7bff91178ba5025fc176e6c5015104a7084fee08
Vikram Patil <vikram.vvp@gmail.com>
2017-07-09 20:35:25 +0800

    complete as per instructions

---
+++ src/CreateContact.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
+
+class CreateContact extends Component  {
+  static propTypes = {
+  }
+  state = {
+  }
+
+  handleSubmit =(e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, {hash:true})
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values)
+    }
+
+  }
+  render() {
+    return(
+      <div>
+        <Link className="close-create-contact" to="/">Close</Link>
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
+          <ImageInput 
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="name" />
+            <input type="text" name="email" placeholder="email" />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateContact;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,21 +1,75 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import propTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
-class ListContacts extends Component {
+class ListContacts extends Component  {
+  static propTypes = {
+    contacts: propTypes.array.isRequired,
+    onDeleteContact: propTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query.trim()})
+  }
+
+  clearQuery = () => {
+    this.setState({query: ''})
+  }
+
   render() {
-    console.log(this.props.contacts)
+    const {contacts, onDeleteContact} = this.props;
+    const {query} = this.state;
+    let showingContacts;
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingContacts = contacts.filter((contact) => match.test(contact.name));
+    }
+    else {
+      showingContacts = contacts;
+    }
+
+    showingContacts.sort(sortBy('name'))
+
     return(
-      <ol className='contact-list'>
-      {this.props.contacts.map((contact) => (
-        <li key={contact.name} className='contact-list-item'>
-          <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}} />
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input 
+            className='search-contacts'
+            type='text'
+            placeholder='Search Contacts'
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+          <Link to="/create"
+            className='add-contact'
+          >Add Contact</Link>
+        </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+              <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+              <button onClick={this.clearQuery} >Show All</button>
           </div>
-          <button className='contact-remove'>Remove</button>
-        </li>
-      ))}
-      </ol>
+        )}
+
+        <ol className='contact-list'>
+        {showingContacts.map((contact) => (
+          <li key={contact.name} className='contact-list-item'>
+            <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}} />
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => onDeleteContact(contact)} className='contact-remove'>Remove</button>
+          </li>
+        ))}
+        </ol>
+      </div>
     );
   }
 }
